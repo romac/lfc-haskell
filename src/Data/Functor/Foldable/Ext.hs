c0cab77890ffcfa0a3ee174cc3a3c25781bd74f1
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-01-06 23:40:49 +0100

    Add monadic ana- and futumorphisms.

--- src/Data/Functor/Foldable/Ext.hs
+++ src/Data/Functor/Foldable/Ext.hs
@@ -1,11 +1,24 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
 
 module Data.Functor.Foldable.Ext
   ( cataM
   ) where
 
-import Data.Functor.Foldable (Base, Recursive, project)
+import Data.Functor.Foldable (Base, Recursive, Corecursive, project, embed, ana)
+import Control.Monad (liftM, join, (<=<))
+import Control.Monad.Free (Free(..))
 
 cataM :: (Recursive t, Monad m, Traversable (Base t)) => (Base t a -> m a) -> t -> m a
-cataM f x = f =<< traverse (cataM f) (project x)
+cataM alg t = alg =<< traverse (cataM alg) (project t)
+
+anaM :: (Corecursive t, Traversable (Base t), Monad m) => (a -> m (Base t a)) -> a -> m t
+anaM coalg = go
+  where go a = fmap embed (coalg a >>= mapM go)
+
+futuM :: (Corecursive t, Traversable (Base t), Monad m) => (a -> m (Base t (Free (Base t) a))) -> a -> m t
+futuM coalg = anaM go . Pure
+  where
+    go (Pure a)  = coalg a
+    go (Free fa) = return fa
 
